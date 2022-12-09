50abb11ef9f5be06a60983afa8100442615cfb13
Kishor H Channal <kishorchannal5@gmail.com>
2022-12-08 21:28:08 -0500

    adding Doctor functionality in Hospital

--- src/Hospital/Doctor/DoctorDirectory.java
+++ src/Hospital/Doctor/DoctorDirectory.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Hospital.Doctor;
+import java.util.ArrayList; 
 
 /**
  *
- * @author chinm
+ * @author kishor
  */
 public class DoctorDirectory {
+    
+    private ArrayList<Doctor> doctorList;
+    
+    public DoctorDirectory() {
+        doctorList = new ArrayList<Doctor>();   
+    }
+
+    public ArrayList<Doctor> getHospitalAdminList() {
+        return doctorList;
+    }
+
+    public void setHospitalAdminList(ArrayList<Doctor> hospitalAdminList) {
+        this.doctorList = hospitalAdminList;
+    }
+    
+    public Doctor addHospitalAdmin(){
+        Doctor newhospitaladmin = new Doctor();
+        doctorList.add(newhospitaladmin);
+        return newhospitaladmin;
+    }
+    
+    public void deleteHospitalAdmin(Doctor m){
+        doctorList.remove(m);
+    }
     
 }
